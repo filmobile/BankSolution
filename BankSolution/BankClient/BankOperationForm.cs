80cb7c409a1b622b964e622ce4a1b32a42b5f383
filmobile <filmobile@gmail.com>
2016-10-28 00:40:51 +0300

    change code

--- BankSolution/BankClient/BankOperationForm.cs
+++ BankSolution/BankClient/BankOperationForm.cs
@@ -21,7 +21,10 @@ namespace BankClient
      
         private void BankOperationForm_Load(object sender, EventArgs e)
         {
-        
+            Build();
+            var cmd = new GetClientAccountCommand() { UserLogin = Client.UserLoginIn };
+            foreach (var p in (Client.Execute(cmd) as ClientAccountAnswer).Accounts)
+                dataTable.Rows.Add(p.Id.ToString(), p.Currency.ToString(), p.Balance.ToString());
         }
         private DataTable dataTable;
         public void Build()
@@ -34,11 +37,35 @@ namespace BankClient
         }
         private void bnClientData_Click(object sender, EventArgs e)
         {
-            Build();
+         /*   Build();
             var cmd = new GetClientAccountCommand() { UserLogin = Client.UserLoginIn };
             foreach (var p in (Client.Execute(cmd) as ClientAccountAnswer).Accounts)
-            dataTable.Rows.Add(p.Id.ToString(), p.Currency.ToString(),p.Balance.ToString()); 
+            dataTable.Rows.Add(p.Id.ToString(), p.Currency.ToString(),p.Balance.ToString()); */
            
+        }
+
+        private void btCreateAccount_Click(object sender, EventArgs e)
+        {
+            pnMain1.Controls.Clear();
+            new AccountPanel() { Parent = pnMain1, Dock = DockStyle.Fill };
+        }
+
+        private void btShowBalance_Click(object sender, EventArgs e)
+        {
+            pnMain1.Controls.Clear();
+            new BalancePanel() { Parent = pnMain1, Dock = DockStyle.Fill };
+        }
+
+        private void btChargeBalance_Click(object sender, EventArgs e)
+        {
+            pnMain1.Controls.Clear();
+            new ChargeBalancePanel() { Parent = pnMain1, Dock = DockStyle.Fill };
+        }
+
+        private void btTransferMoney_Click(object sender, EventArgs e)
+        {
+            pnMain1.Controls.Clear();
+            new TransferMoneyPanel() {Parent=pnMain1, Dock=DockStyle.Fill };
         }
     }
 }
